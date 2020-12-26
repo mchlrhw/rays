2392b1c02cd4192499ad9d95e69966b473c2542a
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-12-26 21:23:59 +0000

    Implement Ray

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,99 @@
 use std::fmt;
 
 use indicatif::ProgressBar;
+use na::{Point3, Vector3};
+use nalgebra as na;
 
-const IMAGE_WIDTH: u64 = 256;
-const IMAGE_HEIGHT: u64 = 256;
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
+const IMAGE_WIDTH: u64 = 400;
+const IMAGE_HEIGHT: u64 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u64;
 
-struct Rgb {
-    r: f64,
-    g: f64,
-    b: f64,
+const VIEWPORT_HEIGHT: f64 = 2.0;
+const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+const FOCAL_LENGTH: f64 = 1.0;
+
+type Point = Point3<f64>;
+type Vector = Vector3<f64>;
+
+struct Rgb(Vector3<f64>);
+
+impl Rgb {
+    fn new(r: f64, g: f64, b: f64) -> Self {
+        Self(Vector3::new(r, g, b))
+    }
 }
 
 impl fmt::Display for Rgb {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let Self { r, g, b } = self;
-
-        let ir = (255.999 * r) as u64;
-        let ig = (255.999 * g) as u64;
-        let ib = (255.999 * b) as u64;
+        let ir = (255.999 * self.0.x) as u64;
+        let ig = (255.999 * self.0.y) as u64;
+        let ib = (255.999 * self.0.z) as u64;
 
         write!(f, "{} {} {}", ir, ig, ib)
     }
 }
 
+impl std::ops::Mul<Rgb> for f64 {
+    type Output = Rgb;
+
+    fn mul(self, other: Rgb) -> Self::Output {
+        Rgb(self * other.0)
+    }
+}
+
+impl std::ops::Add<Rgb> for Rgb {
+    type Output = Rgb;
+
+    fn add(self, other: Rgb) -> Self::Output {
+        Rgb(self.0 + other.0)
+    }
+}
+
+struct Ray {
+    origin: Point,
+    direction: Vector,
+}
+
+impl Ray {
+    fn at(&self, t: f64) -> Point {
+        self.origin + (t * self.direction)
+    }
+}
+
+fn ray_colour(ray: &Ray) -> Rgb {
+    let t = 0.5 * (ray.direction.normalize().y + 1.0);
+
+    ((1.0 - t) * Rgb::new(1.0, 1.0, 1.0)) + (t * Rgb::new(0.5, 0.7, 1.0))
+}
+
 fn main() {
-    let progress = ProgressBar::new(IMAGE_HEIGHT);
+    // Camera
+
+    let origin = Point::new(0.0, 0.0, 0.0);
+    let horizontal = Vector::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let vertical = Vector::new(0.0, VIEWPORT_HEIGHT, 0.0);
+    let lower_left_corner =
+        origin - (horizontal / 2.0) - (vertical / 2.0) - Vector::new(0.0, 0.0, FOCAL_LENGTH);
+
+    // Render
 
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
+    let progress = ProgressBar::new(IMAGE_HEIGHT);
     for j in (0..IMAGE_HEIGHT).rev() {
         progress.inc(1);
         for i in 0..IMAGE_WIDTH {
-            let pixel_colour = Rgb {
-                r: i as f64 / (IMAGE_WIDTH - 1) as f64,
-                g: j as f64 / (IMAGE_HEIGHT - 1) as f64,
-                b: 0.25,
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+
+            let ray = Ray {
+                origin,
+                direction: lower_left_corner + (u * horizontal) + (v * vertical) - origin,
             };
+            let pixel_colour = ray_colour(&ray);
 
             println!("{}", pixel_colour);
         }
     }
-
     progress.finish();
 }

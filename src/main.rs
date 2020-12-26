ae24afbdbe9bf9c014fe4c730abd83fa23cf4ed4
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-12-26 20:05:39 +0000

    Define Rgb type with Display impl

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use std::fmt;
+
 use indicatif::ProgressBar;
 
 const IMAGE_WIDTH: u64 = 256;
 const IMAGE_HEIGHT: u64 = 256;
+
+struct Rgb {
+    r: f64,
+    g: f64,
+    b: f64,
+}
+
+impl fmt::Display for Rgb {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self { r, g, b } = self;
+
+        let ir = (255.999 * r) as u64;
+        let ig = (255.999 * g) as u64;
+        let ib = (255.999 * b) as u64;
+
+        write!(f, "{} {} {}", ir, ig, ib)
+    }
+}
 
 fn main() {
     let progress = ProgressBar::new(IMAGE_HEIGHT);
@@ -11,16 +31,15 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         progress.inc(1);
         for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
+            let pixel_colour = Rgb {
+                r: i as f64 / (IMAGE_WIDTH - 1) as f64,
+                g: j as f64 / (IMAGE_HEIGHT - 1) as f64,
+                b: 0.25,
+            };
 
-            let ir = (255.999 * r) as u64;
-            let ig = (255.999 * g) as u64;
-            let ib = (255.999 * b) as u64;
-
-            println!("{} {} {}", ir, ig, ib);
+            println!("{}", pixel_colour);
         }
     }
+
     progress.finish();
 }

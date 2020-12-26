21225a72ada328d1ca08fb35b86acdc3219ef18d
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-12-26 12:38:13 +0000

    Add a progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use indicatif::ProgressBar;
+
 const IMAGE_WIDTH: u64 = 256;
 const IMAGE_HEIGHT: u64 = 256;
 
 fn main() {
+    let progress = ProgressBar::new(IMAGE_HEIGHT);
+
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        progress.inc(1);
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -17,4 +22,5 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+    progress.finish();
 }

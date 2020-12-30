b5c7f42e7b9ea7192a8749db464c46c2d7936651
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-12-30 12:43:14 +0000

    Render zooming in

--- src/main.rs
+++ src/main.rs
@@ -390,7 +390,7 @@ fn main() {
         Point::new(-2.0, 2.0, 1.0),
         Point::new(0.0, 0.0, -1.0),
         Vector::new(0.0, 1.0, 0.0),
-        90.0,
+        20.0,
         ASPECT_RATIO,
     );
 
